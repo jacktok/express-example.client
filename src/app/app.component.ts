dd39d4b9f51e658c084b2eb2a744120f4758677b
jacky.cm <anan.untha@gmail.com>
2019-06-28 15:43:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {
+    this.response = http.get(this.url);
+  }
+
+  url: string = 'http://localhost:3000';
+
   title = 'express-example-client';
+
+  response: Observable<object>;
+
+  upload(files: FileList) {
+    const file = files[0];
+    let formData = new FormData();
+    formData.append('upload', file);
+
+    formData.append('name', 'jacktok');
+    //
+    // const data = {
+    //   test: "val1",
+    //   test2: "val2",
+    //   u: formData
+    // }
+    this.response = this.http.post(
+      'http://localhost:3000/upload',
+      formData);
+  }
 }
